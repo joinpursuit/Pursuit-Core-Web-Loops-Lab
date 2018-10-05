687951089407fcdaf68d479e7ffde504003bc249
Andres.C <andrescabrera@pursuit.org>
2018-10-04 23:43:56 -0400

    loops hw

---
+++ loopexercise.js
@@ -0,0 +1,48 @@
+// 1.
+//WHILE LOOP
+/*let num = 20
+while (num >= 15) {
+  console.log('the number is: ' + num)
+  num -= 1
+}
+
+// FOR LOOP
+for (let num = 10; num >= 1; num -= 1) {
+  console.log(num)
+} */
+
+// 2. EVEN OR ODD
+//FOR LOOP
+//
+// for (let i = 0; i <= 6; i++){
+//   if (i % 2 === 0){
+//     console.log(i + ' is even')
+//   } else {
+//     console.log(i + ' is odd')
+//   }
+// }
+// console.log("#############################")
+// // WHILE LOOP
+// let num = 10;
+// while(num >= 0) {
+//  if(num % 2 === 0) {
+//    console.log(num + ' is even')
+//  } else {
+//    console.log ( num + ' is odd')
+//  }
+//
+//  num--
+// }
+
+// 3. multiply
+//FOR LOOP
+// let num = 10
+// for (let i = 0; i <= num; i++) {
+//   console.log(i + "*" + 9 + " = " + (i*9))
+// }
+// WHILE LOOP
+// let num = 0;
+// while (num <= 10) {
+//   console.log (num + "*" + 9 + " = " + (num*9))
+// num += 1
+// }
