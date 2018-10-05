66ecfe0e6bee1f9b4b566ddb4974a163a623ef8f
jungraejang <jungraejang@pursuit.org>
2018-10-04 21:48:28 -0400

    loops_assignment

---
+++ loops_assignment.js
@@ -0,0 +1,75 @@
+1. Question
+// let num = 128
+// while (num >= 1) {
+//   console.log(num)
+//   num -= 1
+// }
+//
+//
+// for (let num = 124; num >= 1; num -= 1) {
+//   console.log(num)
+// }
+// ////////////////////////////////////////
+2. Question
+// let num = 0
+// while(num <= 100) {
+//   if(num % 2 === 0) {
+//     console.log(num + ' is even')
+//   } else {
+//     console.log(num + ' is odd')
+//   }
+//   num++
+// }
+// //////////////////////////////////////
+// for (let num = 0; num <= 100; num += 1) {
+//   if(num % 2 === 0) {
+//     console.log(num + ' is even')
+//   } else {
+//     console.log(num + ' is odd')
+//   }
+// }
+/////////////////////////////////////////
+3. Question
+// let num = 0
+// while(num <= 180) {
+//   if(num % 9 === 0) {
+//     console.log(num)
+//   }
+//   num++
+// }
+// //////////////////////////////////////////
+// for (let num = 0; num <= 180; num += 1) {
+//   if(num % 9 === 0) {
+//     console.log(num)
+//   }
+// }
+//////////////////////////////////////////////
+4. Question
+// let num = 1
+// while(num <= 100) {
+//   if(num % 3 === 0) {
+//     console.log('Fizz')
+//   } if(num % 5 === 0) {
+//     console.log('Buzz')
+//   } if(num % 5 != 0 && num % 3 != 0) {
+//     console.log(num)
+//   }
+//   num++
+// }
+///////////////////////////////////////////////
+5. Question
+// let num = 1
+// while(num <= 100) {
+//   if (num % 3 === 0 && num % 5 === 0) {
+//     console.log('FizzBuzz')
+//   }
+//   else if(num % 3 === 0) {
+//     console.log('Fizz')
+//   } else if(num % 5 === 0) {
+//     console.log('Buzz')
+//   } if(num % 5 != 0 && num % 3 != 0) {
+//     console.log(num)
+//   }
+//   num++
+// }
+//////////////////////////////////////////////////
