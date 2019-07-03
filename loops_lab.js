9d742c23ce86c36d5ed6e18cdf819ce065b32536
tawnyblvd <margara.sees@gmail.com>
2019-07-02 20:44:19 -0400

    correct some questions that had been changed overnight

--- loops_lab.js
+++ loops_lab.js
@@ -1,17 +1,27 @@
 // 1. Write a `while` loop and a `for` loop that takes the variable "num" and logs all the numbers, descending, between "num" and 1.
-let num = 24;
-
-while (num-- && num >= 2) {
-  console.log(num);
-}
-
-for (num >= 2; num-= 1;) {
-  console.log(num);
-}
+// let num = 24;
+//
+// while (num-- && num >= 2) {
+//   console.log(num);
+// }
+//
+// for (num >= 2; num-= 1;) {
+//   console.log(num);
+// }
 
 // 2. Write a `while` loop and a `for` loop that takes the variable "max", and iterates over all numbers from 0 to "max".
 // For each iteration, it will check if the current number is even or odd, and log that to the screen (e.g. "2 is even")
-//
+let max = 32;
+
+// for (let max = 0; max <= 32;) {
+//   if (max % 2 === 0) {
+//     console.log(max + " is even.");
+//   } else {
+//     console.log(max + " is odd.");
+//   }
+//   max ++;
+// }
+
 
 
 // 3. Write a `while` loop and a `for` loop that takes the variable "x" and iterates over all numbers from 0 to "x".
@@ -24,76 +34,19 @@ for (num >= 2; num-= 1;) {
 //       Find the final number and increment the loop by 9.
 //     </details>
 //
-// 4. Write a loop that uses console.log to log all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, log "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), log "Buzz" instead.
+// 4. Write code that prints/logs all the numbers that end in a 5 from 1 to 100, exclusive.
+
+// 5. Without running/executing your code, how many times will the loop below run? Explain why.
 //
-// 5. Modify your program to log "FizzBuzz", for numbers that are divisible by both 3 and 5 (still log "Fizz" or "Buzz" for numbers divisible by only one of those).
+// let i = 5;
 //
-// ### Bonus:
+// while (i > 3) {
+//     i += 1
+// }
 //
-// 1. Write a program that would log the [lyrics of the song 99 Bottles of Beer](http://www.99-bottles-of-beer.net/lyrics.html). This is the first verse of the song:
+// // Your explanation here
 //
-//   ```
-//   99 bottles of beer on the wall, 99 bottles of beer.
-//   Take one down, pass it around, 98 bottles of beer on the wall.
-//   ```
+// 6. Write a loop that uses console.log to log all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, log "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), log "Buzz" instead.
 //
-//   This verse is repeated, each time with one less bottle, until the number of bottles is 0. When the number of bottles is 2, the verse is:
+// 7. Modify your program to log "FizzBuzz", for numbers that are divisible by both 3 and 5 (still log "Fizz" or "Buzz" for numbers divisible by only one of those).
 //
-//   ```
-//   2 bottles of beer on the wall, 2 bottles of beer.
-//   Take one down, pass it around, 1 bottle of beer on the wall.
-//   ```
-//
-//   In the last line, the word bottles (plural), is  replaced with bottle (singular)
-//
-//   When the number of bottles is 1, the verse is:
-//
-//   ```
-//   1 bottle of beer on the wall, 1 bottle of beer.
-//   Take one down, pass it around, No more bottle of beer on the wall.
-//   ```
-//
-// ___
-//
-// 2. Use the `assignGrade` function (given below). Write a function that logs each number from 60 - 100 along with its corresponding letter score.
-// Exp For each number from 81 to 90, log B, like so:
-//
-// ```js
-// 60 - F
-// ...
-// 81 - B
-// 82 - B
-// 83 - B
-// ...
-// 93 - A
-// 94 - A
-// ...
-// ```
-//
-// ```js
-// function assignGrade(score) {
-//     if (score > 90) {
-//         return 'A';
-//     } else if (score > 80) {
-//         return 'B';
-//     } else if (score > 70) {
-//         return 'C';
-//     } else if (score > 65) {
-//         return 'D';
-//     } else {
-//         return 'F';
-//     };
-// };
-// ```
-// <details>
-// <summary>
-//   <b>Hint</b>
-// </summary>
-//   Explore this:
-//
-//   ```js
-//   let grade = assignGrade(80)
-//   console.log(grade)
-//   ```
-//   What is happening here? Can you explain it?
-// </details>
