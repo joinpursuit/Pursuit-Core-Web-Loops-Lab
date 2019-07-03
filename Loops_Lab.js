dfc651f3e111be67f98a41ee1d8522599226d786
AminesCodes <aminebensalem@pursuit.org>
2019-07-02 21:38:47 -0400

    Adding the multiplication table

--- Loops_Lab.js
+++ Loops_Lab.js
@@ -198,7 +198,7 @@ for (let i = 1; i <= N; i++){
   for (let j = 1; j <= N; j++){
       console.log(ast);
 }
-
+console.log("----------------------------------------- \n");
 
 // Bonus4:
 // Multiplication table with only two loops
