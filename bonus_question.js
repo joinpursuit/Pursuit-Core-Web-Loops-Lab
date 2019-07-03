a47b7f9430f994412ed8e6909438c4660d2fb4f6
PeterFiorentino <peterfiorentino@pursuit.org>
2019-07-02 22:03:31 -0400

    Peter Fiorentino

---
+++ bonus_question.js
@@ -0,0 +1,6 @@
+for (let n = 1; n <= 12; n++) {
+  console.log("\nMultiplication Table for " + n + "\n");
+  for (let i = 1; i <= 10; i++) {
+    console.log(n + " * " + i + " = " + (n * i));
+  }
+}
