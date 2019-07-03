d280258892f424d1dc48de557e95508cdba0f151
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-07-02 22:10:59 -0400

    Finished super bonus

---
+++ superBonus.js
@@ -0,0 +1,8 @@
+console.log('Super Bonus.--------------');
+
+for (num = 0; num <= 12; num++) {
+  console.log('*~~~Multiples of ' + num + '~~~*');
+  for (num2 = 0; num2 <= 12; num2++) {
+    console.log(num + ' * ' + num2 + ' = ' + num2 * num);
+  }
+}
