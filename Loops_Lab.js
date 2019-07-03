1b68125afc92eebed1aacdbf4f626742ee6b40be
AminesCodes <aminebensalem@pursuit.org>
2019-07-02 21:44:17 -0400

    rearranged the display/console log

--- Loops_Lab.js
+++ Loops_Lab.js
@@ -203,7 +203,7 @@ console.log("----------------------------------------- \n");
 // Bonus4:
 // Multiplication table with only two loops
 for (let i = 1; i <= 10; i++){
-  console.log(i, "Multiplication table \n");
+  console.log("\n", i, "Multiplication table");
   for (let j = 1; j <= 10; j++){
     console.log(i, " x ", j, " = ", i*j);
   }
