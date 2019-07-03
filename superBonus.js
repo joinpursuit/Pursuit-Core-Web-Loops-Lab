d51afc2bbd4a3b92d1609efc1b605cae1e4f4ae7
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-07-02 22:12:43 -0400

    multiples 1 to 10

--- superBonus.js
+++ superBonus.js
@@ -1,6 +1,6 @@
 console.log('Super Bonus.--------------');
 
-for (num = 0; num <= 12; num++) {
+for (num = 0; num <= 10; num++) {
   console.log('*~~~Multiples of ' + num + '~~~*');
   for (num2 = 0; num2 <= 12; num2++) {
     console.log(num + ' * ' + num2 + ' = ' + num2 * num);
