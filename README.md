236f6e9494573d03569616ccc7ea4d410db6a497
Cam Jess <camillejessamy@pursuit.org>
2019-07-02 22:49:50 -0400

    answers to loops lab

--- README.md
+++ README.md
@@ -1,33 +1,54 @@
-# Loops Lab
+<!-- # Loops Lab
 
 To complete this lab, do the following:
 
 1. Fork this repo
 2. Clone **your** forked repo
 3. Create a file called `loops_lab.js` inside your cloned repo
-4. Copy the description of the exercise as a comment  
+4. Copy the description of the exercise as a comment
 5. Underneath the commented description write your code.
 6. Ensure your solutions work properly!
 7. Open PR against this repo.
 
-___
+---
 
 ## Exercises
 
-1. Write a `while` loop and a `for` loop that takes the variable "num" and logs all the numbers, descending, between "num" and 1.
+1. Write a `while` loop and a `for` loop that takes the variable "num" and logs all the numbers, descending, between "num" and 1. -->
+
+  let num = 23;
+while (num >= 1) {
+console.log(num);
+num -= 1;
+}
+console.log("--------------");
+for (let num = 23; num >= 1; num -= 1) {
+console.log(num);
+}
 
 2. Write a `while` loop and a `for` loop that takes the variable "max", and iterates over all numbers from 0 to "max".
-For each iteration, it will check if the current number is even or odd, and log that to the screen (e.g. "2 is even")
+   For each iteration, it will check if the current number is even or odd, and log that to the screen (e.g. "2 is even")
+
+let num = 0;
+while (num <= 42){
+if (num % 2 === 0) {
+console.log(num + " is even.");
+} else {
+console.log(num + " is odd.");
+}
+num += 1;
+}
 
 3. Write a `while` loop and a `for` loop that takes the variable "x" and iterates over all numbers from 0 to "x".
-For each iteration of the loop, it will multiply the number of "x" by 9 and log the result (e.g. "2 * 9 = 18").
-  _Bonus_ think of another way to solve it.
-    <details>
-      <summary>
-        Hint
-      </summary>
-      Find the final number and increment the loop by 9.
-    </details>
+   For each iteration of the loop, it will multiply the number of "x" by 9 and log the result (e.g. "2 \* 9 = 18").
+   _Bonus_ think of another way to solve it.
+
+   <details>
+   <summary>
+   Hint
+   </summary>
+   Find the final number and increment the loop by 9.
+   </details>
 
 4. Write code that prints/logs all the numbers that end in a 5 from 1 to 100, **exclusive**.
 
@@ -37,7 +58,7 @@ For each iteration of the loop, it will multiply the number of "x" by 9 and log 
 let i = 5;
 
 while (i > 3) {
-    i += 1
+  i += 1;
 }
 
 // Your explanation here
@@ -51,31 +72,31 @@ while (i > 3) {
 
 1. Write a program that would log the [lyrics of the song 99 Bottles of Beer](http://www.99-bottles-of-beer.net/lyrics.html). This is the first verse of the song:
 
-  ```
-  99 bottles of beer on the wall, 99 bottles of beer.
-  Take one down, pass it around, 98 bottles of beer on the wall.
-  ```
+```
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down, pass it around, 98 bottles of beer on the wall.
+```
 
-  This verse is repeated, each time with one less bottle, until the number of bottles is 0. When the number of bottles is 2, the verse is:
+This verse is repeated, each time with one less bottle, until the number of bottles is 0. When the number of bottles is 2, the verse is:
 
-  ```
-  2 bottles of beer on the wall, 2 bottles of beer.
-  Take one down, pass it around, 1 bottle of beer on the wall.
-  ```
+```
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down, pass it around, 1 bottle of beer on the wall.
+```
 
-  In the last line, the word bottles (plural), is  replaced with bottle (singular)
+In the last line, the word bottles (plural), is replaced with bottle (singular)
 
-  When the number of bottles is 1, the verse is:
+When the number of bottles is 1, the verse is:
 
-  ```
-  1 bottle of beer on the wall, 1 bottle of beer.
-  Take one down, pass it around, No more bottle of beer on the wall.
-  ```
+```
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down, pass it around, No more bottle of beer on the wall.
+```
 
-___
+---
 
 2. Use the `assignGrade` function (given below). Write a function that logs each number from 60 - 100 along with its corresponding letter score.
-Exp For each number from 81 to 90, log B, like so:
+   Exp For each number from 81 to 90, log B, like so:
 
 ```js
 60 - F
@@ -91,33 +112,36 @@ Exp For each number from 81 to 90, log B, like so:
 
 ```js
 function assignGrade(score) {
-    if (score > 90) {
-        return 'A';
-    } else if (score > 80) {
-        return 'B';
-    } else if (score > 70) {
-        return 'C';
-    } else if (score > 65) {
-        return 'D';
-    } else {
-        return 'F';
-    };
-};
+  if (score > 90) {
+    return "A";
+  } else if (score > 80) {
+    return "B";
+  } else if (score > 70) {
+    return "C";
+  } else if (score > 65) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
 ```
+
 <details>
 <summary>
   <b>Hint</b>
 </summary>
   Explore this:
 
-  ```js
-  let grade = assignGrade(80)
-  console.log(grade)
-  ```
-  What is happening here? Can you explain it?
+```js
+let grade = assignGrade(80);
+console.log(grade);
+```
+
+What is happening here? Can you explain it?
+
 </details>
 
-___
+---
 
 3. Given an integer N draw a square of N x N asterisks. Look at the examples.
 
@@ -125,6 +149,7 @@ Example 1:
 Input: `let N = 2`
 
 Output:
+
 ```js
 **
 **
@@ -134,6 +159,7 @@ Example 2:
 Input: `let N = 3`
 
 Output:
+
 ```js
 ***
 ***
---
+++ loops_lab.js
@@ -0,0 +1,203 @@
+// <!-- # Loops Lab
+//
+// To complete this lab, do the following:
+//
+// 1. Fork this repo
+// 2. Clone **your** forked repo
+// 3. Create a file called `loops_lab.js` inside your cloned repo
+// 4. Copy the description of the exercise as a comment
+// 5. Underneath the commented description write your code.
+// 6. Ensure your solutions work properly!
+// 7. Open PR against this repo.
+//
+// ---
+//
+// ## Exercises
+//
+// 1. Write a `while` loop and a `for` loop that takes the variable "num" and logs all the numbers, descending, between "num" and 1. -->
+//
+//   let num = 23;
+// while (num >= 1) {
+// console.log(num);
+// num -= 1;
+// }
+// console.log("--------------");
+// for (let num = 23; num >= 1; num -= 1) {
+// console.log(num);
+// }
+//
+// 2. Write a `while` loop and a `for` loop that takes the variable "max", and iterates over all numbers from 0 to "max".
+//    For each iteration, it will check if the current number is even or odd, and log that to the screen (e.g. "2 is even")
+//
+
+//
+// 3. Write a `while` loop and a `for` loop that takes the variable "x" and iterates over all numbers from 0 to "x".
+//    For each iteration of the loop, it will multiply the number of "x" by 9 and log the result (e.g. "2 \* 9 = 18").
+//    _Bonus_ think of another way to solve it.
+// for (let num = 0; num < 36; num += 1) {
+//   console.log(num + ' * 9 =' + num * 9)
+// }
+// console.log('-----------------');
+
+//
+//    <details>
+//    <summary>
+//    Hint
+//    </summary>
+//    Find the final number and increment the loop by 9.
+//    </details>
+//
+// 4. Write code that prints/logs all the numbers that end in a 5 from 1 to 100, **exclusive**.
+// for (x = 0; x < 100; x += 5){
+//   if (x % 2 === 1){
+//     console.log(x);
+//   } else {
+//     console.log("");
+//   }
+// }
+// 5. Without running/executing your code, how many times will the loop below run? Explain why.
+//
+// ```js
+// let i = 5;
+//
+// while (i > 3) {
+//   i += 1;
+// }
+//
+// // Your explanation here
+// >>>It will run on to infinty, because i = 5. Our conditional is met because 5 is greater than 3. The loop will continue running because the loop will continue to increase by 1 each time, and no limit has been given.
+//
+// 6. Write a loop that uses console.log to log all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, log "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), log "Buzz" instead.
+// for (x = 0; x < 100; x += 1) {
+//   if (x % 3 === 0) {
+//     console.log(" Fizz");
+//   } else if (x % 5 === 0) {
+//   console.log(" Buzz");
+// } else {
+//   console.log(x);
+// }
+// }
+// 7. Modify your program to log "FizzBuzz", for numbers that are divisible by both 3 and 5 (still log "Fizz" or "Buzz" for numbers divisible by only one of those).
+ for (x = 0; x < 100; x += 1){
+  if (x % 3 === 0 ){
+    console.log(x + 'Fizz');
+  } else if(x % 5 === 0){
+   console.log(x + 'Buzz');
+ }
+  if (x % 3 === 0 && x % 5 === 0) {
+  console.log(x + 'FizzBuzz');
+} else {console.log(x);
+}
+}
+
+// ### Bonus:
+//
+// 1. Write a program that would log the [lyrics of the song 99 Bottles of Beer](http://www.99-bottles-of-beer.net/lyrics.html). This is the first verse of the song:
+//
+// ```
+// 99 bottles of beer on the wall, 99 bottles of beer.
+// Take one down, pass it around, 98 bottles of beer on the wall.
+// ```
+//
+// This verse is repeated, each time with one less bottle, until the number of bottles is 0. When the number of bottles is 2, the verse is:
+//
+// ```
+// 2 bottles of beer on the wall, 2 bottles of beer.
+// Take one down, pass it around, 1 bottle of beer on the wall.
+// ```
+//
+// In the last line, the word bottles (plural), is replaced with bottle (singular)
+//
+// When the number of bottles is 1, the verse is:
+//
+// ```
+// 1 bottle of beer on the wall, 1 bottle of beer.
+// Take one down, pass it around, No more bottle of beer on the wall.
+// ```
+//
+// ---
+//
+// 2. Use the `assignGrade` function (given below). Write a function that logs each number from 60 - 100 along with its corresponding letter score.
+//    Exp For each number from 81 to 90, log B, like so:
+//
+// ```js
+// 60 - F
+// ...
+// 81 - B
+// 82 - B
+// 83 - B
+// ...
+// 93 - A
+// 94 - A
+// ...
+// ```
+//
+// ```js
+// function assignGrade(score) {
+//   if (score > 90) {
+//     return "A";
+//   } else if (score > 80) {
+//     return "B";
+//   } else if (score > 70) {
+//     return "C";
+//   } else if (score > 65) {
+//     return "D";
+//   } else {
+//     return "F";
+//   }
+// }
+// ```
+//
+// <details>
+// <summary>
+//   <b>Hint</b>
+// </summary>
+//   Explore this:
+//
+// ```js
+// let grade = assignGrade(80);
+// console.log(grade);
+// ```
+//
+// What is happening here? Can you explain it?
+//
+// </details>
+//
+// ---
+//
+// 3. Given an integer N draw a square of N x N asterisks. Look at the examples.
+//
+// Example 1:
+// Input: `let N = 2`
+//
+// Output:
+//
+// ```js
+// **
+// **
+// ```
+//
+// Example 2:
+// Input: `let N = 3`
+//
+// Output:
+//
+// ```js
+// ***
+// ***
+// ***
+// ```
+//
+// <details>
+//   <summary>
+//     Hint 1
+//   </summary>
+//   Try printing/logging a single line of * first.
+// </details>
+//
+// <details>
+//   <summary>
+//     Hint 2
+//   </summary>
+//   You will need 2 loops for this.
+// </details>
