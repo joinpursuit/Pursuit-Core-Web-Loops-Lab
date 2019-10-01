230cd99f400fcc796311242cc3b175b7c7137fad
Jay Fowler <jcafowler@gmail.com>
2019-09-30 21:40:32 -0400

    Problem2A

--- loops_lab.js
+++ loops_lab.js
@@ -12,20 +12,46 @@
 // let num = 8
 
 // **** A ****
-let num = 50
-while (num > 2){
-    num--
-    console.log(num)
-}
+// let num = 50
+// while (num > 2){
+//     num--
+//     console.log(num)
+// }
 
 // OR *** Unsure if we should include or exlcude num and 1 
 
-let numB = 50
-while (numB >= 1){
-    console.log(numB)
-    numB--
+// let numB = 50
+// while (numB >= 1){
+//     console.log(numB)
+//     numB--
+// }
+// // **** B ****
+// for(let numC = 30; numC >= 1; numC--){
+//     console.log(numC)
+// }
+
+// # Problem Two
+
+// a. Write a `while` loop that takes the variable "max", and iterates over all numbers from 0 to "max". 
+// For each iteration, it will check if the current number is even or odd, and log that to the screen (e.g. "2 is even")
+
+// b. Use a `for` loop instead
+
+// Example: 
+
+// ```js
+// let max = 3
+
+// **** A. ****
+let max = 100;
+
+while (max <= 100 && max >= 0) {
+    if (max % 2 === 0) {
+        console.log(max + " is even");
+    } else {
+        console.log(max + " is odd");
+    }
+    max--;
 }
-// **** B ****
-for(let numC = 30; numC >= 1; numC--){
-    console.log(numC)
-}
+
+// ***** B. *****
