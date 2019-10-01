ed06d3b9255e27f7eebc8903709e135b94a52187
Cassidy Beni <cassidybeni@pursuit.org>
2019-09-30 21:58:21 -0400

    update 3

--- loops_lab.js
+++ loops_lab.js
@@ -15,3 +15,30 @@ for (let num2 = 10; num2 >= 1; num2 -= 1){
 
     console.log ("The number is:"+num2);
 };
+
+//a. Write a `while` loop that takes the variable "max", and iterates over all numbers from 0 to "max". 
+//For each iteration, it will check if the current number is even or odd, and log that to the screen (e.g. "2 is even")
+let max = 8
+let counter = 0
+
+
+while (counter <= max) {
+    if (counter%2 === 0){
+    console.log(counter + " is even")
+    } else[
+        console.log(counter + " is odd")
+    ]
+        counter += 1
+
+}
+
+
+
+
+
+
+
+
+
+
+
