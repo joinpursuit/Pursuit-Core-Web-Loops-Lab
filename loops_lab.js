b4f407df2d8c2a457a213563b59fe47261968908
Cassidy Beni <cassidybeni@pursuit.org>
2019-09-30 21:33:39 -0400

    update

---
+++ loops_lab.js
@@ -0,0 +1,9 @@
+//a. Write a `while` loop that takes the variable `num` and
+// logs all the numbers, descending, between "num" and 1
+let num = 5;
+
+while ( num >= 1) { 
+    console.log("the number is: " + num)
+    num -= 1
+}
+
