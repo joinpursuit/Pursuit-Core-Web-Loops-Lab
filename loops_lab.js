aff3094ae70fa2aac986cbaaa0b5d53ae63a1508
Oblivion <kiaraedwards@pursuit.org>
2019-10-01 16:56:56 -0400

    heres the work

---
+++ loops_lab.js
@@ -0,0 +1,51 @@
+let num = 10;
+  
+  
+  while (num >= 1){
+    num -= 1;
+      console.log(num)
+  }
+
+  
+  
+  
+  
+  for (let i = 10; i >=1; i -= 1){
+    
+    console.log(i)
+}
+
+
+
+let num2 = 20;
+
+while (num2 >=0){
+    
+        if (num2 % 2 === 0){
+            console.log("number is even");
+        } else
+        {
+            console.log("number is odd")
+        }
+        num2-- 
+}
+
+for (let j= 0; j <= 10; j++){
+    if(j % 2 === 0){
+        console.log(j + " is even");
+    } else {
+        console.log(j + " is odd");
+    }
+}
+
+
+
+let x = 45
+
+while (x >=0){
+
+}
+
+
+
+
