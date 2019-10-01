c07565c8256bd5f8d0f38461070b2fef2c965bb2
Kelvin <kelvinarellano@pursuit.org>
2019-09-30 21:24:23 -0400

    first done

--- loops_lab.js
+++ loops_lab.js
@@ -1,11 +1,13 @@
 // ## Problem One 
 // a. Write a `while` loop that takes the variable `num` and logs all the numbers, descending, between "num" and 1
-
+// let num = 10
+// while (num >=1) {
+//     console.log(num)
+//     num--
+// }
 // b. Write a `for` loop that takes the variable `num` and logs all the numbers, descending, between "num" and 1
-let num = 10
-while (num >=1) {
-    console.log(num)
-    num--
+for (let i = 10; i >=1; i--){
+    console.log(i)
 }
 
 // ## Problem Two
