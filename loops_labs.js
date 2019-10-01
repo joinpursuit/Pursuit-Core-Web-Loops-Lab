f1752bf7836d833386f5e66512d693b9522aadcf
Jovanni <jovanniluna@pursuit>
2019-09-30 21:31:48 -0400

    Problem 1 solved

---
+++ loops_labs.js
@@ -0,0 +1,12 @@
+// a. Write a `while` loop that takes the variable `num` and logs all the numbers, descending, between "num" and 1
+let num = 6
+
+while (num >= 1) {
+    console.log(num);
+    num --;
+}
+//b. Write a `for` loop that takes the variable `num` and logs all the numbers, descending, between "num" and 1
+
+for (let num = 6; num >= 1; num --) {
+    console.log(num)
+}
