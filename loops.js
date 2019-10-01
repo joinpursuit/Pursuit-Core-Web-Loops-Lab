a02ecf05d39ca3a67056cc0a4ba1712e05e6b0bf
rafidhossain1995 <rafidhossain@pursuit.org>
2019-09-30 21:34:48 -0400

    loop lap

---
+++ loops.js
@@ -0,0 +1,31 @@
+// let num = 50
+// while (num >=1){
+//     console.log(num)
+//     num-=1
+// }
+
+// b)
+// for (num1 = 50; num1 >=1; num1-=1){
+//     console.log(num1)
+// }
+
+// problem 2
+
+// let max= 1
+// while (max <=100){
+// if (max %2 === 0){
+//   console.log(max+ " " + "even")
+// }else {
+//     console.log(max + " " + "odd")
+//     } max ++
+// }
+
+// problem 2 part 2
+
+for (max1 = 1; max1 <= 100; max1 ++) {
+    if (max1 %2 === 0){
+        console.log(max1 + " " + "even")
+    } else {
+        console.log(max1 + " " + "odd")
+    }
+}
