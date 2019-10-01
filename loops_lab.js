103398d53b735a1cf873e4d554cb6f15f18c5cd3
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-01 04:37:21 -0400

    Format the file

--- loops_lab.js
+++ loops_lab.js
@@ -5,10 +5,12 @@
 //     console.log(num)
 //     num--
 // }
+
 // b. Write a `for` loop that takes the variable `num` and logs all the numbers, descending, between "num" and 1
 // for (let i = 10; i >=1; i--){
 //     console.log(i)
 // }
+
 
 // ## Problem Two
 // a. Write a `while` loop that takes the variable "max", and iterates over all numbers from 0 to "max". 
@@ -23,6 +25,7 @@
 //     }
 //     num++
 //}
+
 // b. Use a `for` loop instead
 // let num = 0
 // for (let i = 100; i >= 0; i--){
@@ -35,8 +38,8 @@
 //     num++
 // }
 
-// ## Problem Three
 
+// ## Problem Three
 // a. Write a `while` loop that takes the variable "x" and iterates over all numbers from 0 to "x". For each iteration of the loop, it will multiply the number of "x" by 9 and log the result 
 // let x = 100
 // let num = 0
@@ -51,11 +54,9 @@
 //     console.log(num + " * 9 = " + num * 9)
 // num++
 // }
-// Write a `while` loop and a `for` loop that takes the variable "x" and iterates over all numbers from 0 to "x". For each iteration of the loop, it will multiply the number of "x" by 9 and log the result (e.g. "2 * 9 = 18").
 
 
 // ## Problem Four
-
 // Write code that prints/logs all the numbers that end in a 5 from 1 to 100, **exclusive**.
 // for(let i = 1; i <= 100; i++) {
 //     if(i % 5 === 0 && i % 2 === 1) {
@@ -63,12 +64,11 @@
 //     }
 // }
 
-// ## Problem Five
 
+// ## Problem Five
 // Without running/executing your code, how many times will the loop below run? Explain why.
 
 // let i = 5;
-
 // while (i > 3) {
 //     i += 1
 // }
@@ -78,7 +78,6 @@
 
 
 // ## Problem Six
-
 // Write a loop that uses console.log to log all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, log "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), log "Buzz" instead.
 for(let i = 1; i <= 100; i++) {
     if (i % 3 === 0) {
@@ -90,50 +89,30 @@ for(let i = 1; i <= 100; i++) {
     }
 }
 
-// ## Problem Seven
 
+// ## Problem Seven
 // Modify your program to log "FizzBuzz", for numbers that are divisible by both 3 and 5 (still log "Fizz" or "Buzz" for numbers divisible by only one of those).
 
 
 // ## Problem Eight
-
 // Write a program that would log the [lyrics of the song 99 Bottles of Beer](http://www.99-bottles-of-beer.net/lyrics.html). This is the first verse of the song:
-
-//   ```
 //   99 bottles of beer on the wall, 99 bottles of beer.
 //   Take one down, pass it around, 98 bottles of beer on the wall.
-//   ```
 
 // This verse is repeated, each time with one less bottle, until the number of bottles is 0. When the number of bottles is 2, the verse is:
-
-//   ```
 //   2 bottles of beer on the wall, 2 bottles of beer.
 //   Take one down, pass it around, 1 bottle of beer on the wall.
-//   ```
 
 //   In the last line, the word bottles (plural), is  replaced with bottle (singular)
-
 //   When the number of bottles is 1, the verse is:
-
-//   ```
 //   1 bottle of beer on the wall, 1 bottle of beer.
 //   Take one down, pass it around, No more bottle of beer on the wall.
-//   ```
 
 
 // ## Problem Nine
-
 // Given an integer N draw a square of N x N asterisks. Look at the examples.
-
-//   Hint 1 
-//   Try printing/logging a single line of * first.
-
-//   Hint 2
-//   You will need 2 loops for this.
 
 
 // ## Problem Ten
-
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
 // Find the sum of all the multiples of 3 or 5 below 1000.
