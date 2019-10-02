da8df1cc0ac681b886d5dd5babde5eff0d972362
dannyl <danilovlaurent@pursuit.org>
2019-10-01 20:56:07 -0400

    Problem 1 And 2

---
+++ looplab.js
@@ -0,0 +1,48 @@
+// PROBLEM 1
+// Part A
+
+
+let num = 10;
+while (num >= 1){
+console.log(num)
+num-- 
+}
+
+// Part B
+
+for (num = 10; num >= 1; num--){
+    console.log(num);
+}
+
+
+
+// PROBLEM 2 
+// Part A
+
+let max = 10
+let num = 0
+while (num <= max){
+    if (max % 2 === 0){
+        console.log(num + " Is Even");
+    } else {
+        console.log(num + " Is Odd");
+    }
+      num++
+    }   
+    
+    
+// Part B
+
+let num = 0
+for (max = 10; num <= max ; num++){
+    if (num % 2 === 0){
+        console.log(num + " Is Even");
+    } else {
+        console.log(num + " Is Odd");
+    }
+}
+   
+    
+
+
+ 
