e8042a7256a4a77afac3d18ac86837b8f097c154
crystal cardona <crystalcardona@pursuit.org>
2019-10-01 23:44:25 -0400

    Working on Problems 6-9

--- loops_lab.js
+++ loops_lab.js
@@ -3,16 +3,16 @@
 // ## Problem One 
 
 // a. Write a `while` loop that takes the variable `num` and logs all the numbers, descending, between "num" and 1
-// let num = 30;
-// while (num >= 1) {
-//     console.log(num);
-//     num -= 2;
-// }
+let num = 30;
+while (num >= 1) {
+    console.log(num);
+    num -= 2;
+}
 
 // // b. Write a `for` loop that takes the variable `num` and logs all the numbers, descending, between "num" and 1
-// for (let i = 30; i >= 1; i -=2) {
-//     console.log(i)
-// }
+for (let i = 30; i >= 1; i -=2) {
+    console.log(i)
+}
 
 // Example:
 
@@ -38,12 +38,12 @@
 // a. Write a `while` loop that takes the variable "max", and iterates over all numbers from 0 to "max". 
 // For each iteration, it will check if the current number is even or odd, and log that to the screen (e.g. "2 is even")
 
-// let max = 50
-// while (max > 0) {
-//     if (max % 2 === 0)
-//     console.log(max + " is even")
-//     max +=1
-// }
+let max = 50
+while (max > 0) {
+    if (max % 2 === 0)
+    console.log(max + " is even")
+    max +=1
+}
 
 // b. Use a `for` loop instead
 
@@ -70,16 +70,16 @@
 // ## Problem Three
 
 // a. Write a `while` loop that takes the variable "x" and iterates over all numbers from 0 to "x". For each iteration of the loop, it will multiply the number of "x" by 9 and log the result 
-// let x = 0;
-// while (x <= 45){
-//     console.log(x + " " + "*" + " " + "9" + "=" + " " + x*9);
-//     x +=1;
-// }
-// // b. Use a `for` loop instead.
+let x = 0;
+while (x <= 45){
+    console.log(x + " " + "*" + " " + "9" + "=" + " " + x*9);
+    x +=1;
+}
+// b. Use a `for` loop instead.
 
-// for (let x = 0; x <= 45; x +=1) {
-//     console.log(x + " " + "*" + " " + "9" + "=" + " " + x*9)
-// }
+for (let x = 0; x <= 45; x +=1) {
+    console.log(x + " " + "*" + " " + "9" + "=" + " " + x*9)
+}
 // Example:
 
 // ```js
