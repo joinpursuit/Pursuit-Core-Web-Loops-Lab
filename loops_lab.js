4b3d06ba504458670b8d1ffb1aefa33d9b2b5c4e
Jay Fowler <jcafowler@gmail.com>
2019-10-01 21:55:19 -0400

    Complete Problem Ten

--- loops_lab.js
+++ loops_lab.js
@@ -166,3 +166,17 @@ while (N > 0){
     console.log (row)
     N--;
 }
+// ## Problem Ten
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+let sum = 0
+let x = 0 
+
+while (x < 1000){
+    if (x % 3 === 0 || x % 5 === 0){
+        sum += x
+    } x++;
+}
+console.log (sum)
