20cb1106bc48cc6a107d840dbdf40f6636adc438
dannyl <danilovlaurent@pursuit.org>
2019-10-01 21:28:23 -0400

    problem 3

--- looplab.js
+++ looplab.js
@@ -2,47 +2,81 @@
 // Part A
 
 
-let num = 10;
-while (num >= 1){
-console.log(num)
-num-- 
-}
+// let num = 10;
+// while (num >= 1){
+// console.log(num)
+// num-- 
+// }
 
 // Part B
 
-for (num = 10; num >= 1; num--){
-    console.log(num);
-}
+// for (num = 10; num >= 1; num--){
+//     console.log(num);
+// }
 
 
 
 // PROBLEM 2 
 // Part A
 
-let max = 10
-let num = 0
-while (num <= max){
-    if (max % 2 === 0){
-        console.log(num + " Is Even");
-    } else {
-        console.log(num + " Is Odd");
-    }
-      num++
-    }   
+// let max = 10
+// let num = 0
+// while (num <= max){
+//     if (max % 2 === 0){
+//         console.log(num + " Is Even");
+//     } else {
+//         console.log(num + " Is Odd");
+//     }
+//       num++
+//     }   
     
     
 // Part B
 
-let num = 0
-for (max = 10; num <= max ; num++){
-    if (num % 2 === 0){
-        console.log(num + " Is Even");
-    } else {
-        console.log(num + " Is Odd");
-    }
-}
+// let num = 0
+// for (max = 10; num <= max ; num++){
+//     if (num % 2 === 0){
+//         console.log(num + " Is Even");
+//     } else {
+//         console.log(num + " Is Odd");
+//     }
+// }
    
     
 
+
+// PROBLEM 3
+
+// Part A
+
+
+// let y = 0
+// let x = 5
+// while (y <= x){
+//  console.log(y + " *" + " 9" + " =" + y *9); {
+//      y++;
+//  }
+
+//  }
+
+
+// Part B
+
+
+
+// let y = 0
+// for (x = 5; y <= x; y++){
+//     console.log(y + " *" + " 9" + " =" + y *9);
+// }
+
+
+   
+  
+
+
+
+      
+  
+  
 
  
