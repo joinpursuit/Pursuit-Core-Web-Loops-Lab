e0718e458c19f56f477c87666d5d28684f384b83
Danielle R Cherry <daniellecherry@pursuit.org>
2019-10-01 20:17:43 -0400

    Solve Problems 1 & 2

---
+++ test.js
@@ -0,0 +1,13 @@
+// let x = 0
+// let y = 4
+// let z = -1
+
+// if (num === 4 && num === 0 && num === -1) {
+//   console.log(x + ', ' + )
+// }
+
+for (let i = 1000; i > 0; i ++) {
+    if (i % 3 === 0) {
+        console.log(i += i)
+    }
+}
