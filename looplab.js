b371e211caff2f653071a157e1e60be2318d3f0b
dannyl <danilovlaurent@pursuit.org>
2019-10-03 22:39:48 -0400

    problems 6-8

--- looplab.js
+++ looplab.js
@@ -76,13 +76,82 @@
 // Problem 4
 
 
-for(x = 5; x <= 100; x += 10){
-    console.log(x);
+// for(x = 5; x <= 100; x += 10){
+//     console.log(x);
 
-}
+// }
     
       
-  
-  
 
- 
+// PROBLEM 5
+// answered answered in the read me
+
+
+
+// PROBLEM 6
+
+
+// for (let x = 1; x<= 100 ; x++){
+//     if (x % 3 === 0){
+//         console.log("Fiz");
+//     } else if(x % 5 === 0 && !(x % 3 === 0)) {
+//         console.log("buzz");
+//     } else {
+//         console.log(x);
+//     }
+
+//     }
+    
+    
+    
+    
+    
+    
+// PROBLEM 7
+
+for (let x = 1; x <= 100 ; x++){
+    if (x % 3 ===0 && x % 5 === 0){
+       console.log("FizzBuzz");
+    } else if ( x % 3 === 0){
+        console.log("fizz");
+    } else if (x % 5 === 0){
+        console.log("buzz")
+    } else {
+        console.log(x);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// PROBLEM 8
+
+// for (let beer = 99; beer >= 1 ; beer--){
+//     if(beer === 2){
+//         console.log("2 bottles of beer on the wall, 2 bottles of beer.take one down, pass it around, 1 bottle of beer on the wall.");
+//         continue;
+//     } else if (beer === 1){
+//         console.log("1 bottle of beer on the wall, 1 bottle of beer.Take one down, pass it around, No more bottle of beer on the wall.");
+//         continue;
+//     }
+//     console.log(beer + " bottles of beer on the wall," + beer+  " bottles of beer." + " take one down pass it around" + " " + (beer -1) + " bottles of beer on the wall");
+
+// } 
+
+
+
+    
+        
+        
+        
+                
+                
