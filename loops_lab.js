c8804ca9da2aea52de753715e53d8a7ea74f26cc
serami12 <stephanieramirez1@pursuit.org>
2019-11-14 15:30:24 -0500

    answered q-1a

---
+++ loops_lab.js
@@ -0,0 +1,12 @@
+// Loops Lab
+//
+// Problem One
+// a.Write a while loop that takes the variable num and logs all the numbers, descending, between "num"
+// and 1
+
+let numA = 5;
+while (numA > 1) {
+    console.log(numA)
+    numA--;
+}
+console.log(numA)
