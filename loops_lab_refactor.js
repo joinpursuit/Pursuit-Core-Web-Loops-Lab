48e3b78af17f5b672ae67a8ac0fed90d86dcd0a7
cassidybeni <cassidybeni@pursuit.org>
2020-03-08 17:03:23 -0400

    Edit while loop to log numbers 10 - 1 and add for loop

--- loops_lab_refactor.js
+++ loops_lab_refactor.js
@@ -1,7 +1,11 @@
 // Problem One 
 
 let num = 10
-while (num > 1) {
-  num--
-  console.log(num)
+while (num >= 1) {
+    console.log(num)
+    num--
+}
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i)
 }
