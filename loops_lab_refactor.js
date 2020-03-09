61a3f80276bb370c76fb8c9bb0d6c2eb2d7f24de
cassidybeni <cassidybeni@pursuit.org>
2020-03-08 22:05:24 -0400

    Complete problem 2

--- loops_lab_refactor.js
+++ loops_lab_refactor.js
@@ -1,11 +1,36 @@
 // Problem One 
 
+//a.
 let num = 10
 while (num >= 1) {
     console.log(num)
     num--
 }
 
+//b.
 for (let i = 10; i >= 1; i--) {
     console.log(i)
+}
+
+// Problem Two
+
+//a.
+let max = 3
+let number = 0
+while (number <= max) {
+    if (number % 2 === 0){
+        console.log(number + ' is even')
+    } else {
+        console.log(number + ' is odd')
+    }
+    number++
+}
+
+//b.
+for (let i = 0; i <= 3; i++) {
+    if (i % 2 === 0){
+        console.log(i + ' is even')
+    } else {
+        console.log(i + ' is odd')
+    }
 }
